32d0f7f4c59b61fc6489d568536701efe896c814
Daud Khan <daudu6944@gmail.com>
2025-05-16 19:25:57 +0500

    add: protected route functionality

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import { api } from "@/utils";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
 
-const isAuthenticated = false;
-
 export const ProtectedRoute = ({ children }) => {
-  if (!isAuthenticated) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkUserAuthenticated() {
+      try {
+        setLoading(true);
+        const { status, data } = await api.get("/auth/check");
+        if (status === 200) {
+          setUser(data);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    checkUserAuthenticated();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(user);
+
+  if (!user?.userId) {
     return <Navigate to={"/signin"} />;
   }
+
   return <div>{children}</div>;
 };
